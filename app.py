77483a81e8ca0369970081172560460835564dad
vardhanam <vardhanam.daga@gmail.com>
2024-03-05 10:24:44 +0000

    pushing

--- app.py
+++ app.py
@@ -34,17 +34,17 @@ def load_llm():
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16
-    )
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_compute_dtype=torch.bfloat16
+    # )
 
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        quantization_config=bnb_config,
+        # quantization_config=bnb_config,
     )
 
     # Building a LLM text-generation pipeline
@@ -168,7 +168,7 @@ vector_retriever = initialize_vectorstore()
 standalone_question, retrieved_documents, answer = return_chain_elements()
 
 #Title for the streamlit app
-st.title('E2E Help Chatbot')
+st.title('Infinita Help Chatbot')
 
 # Initialize chat history
 if "messages" not in st.session_state:
